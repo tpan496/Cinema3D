f5a4ebdaac5520ede9a3901cfb41831dba76d6a2
tpan496 <tianleip@andrew.cmu.edu>
2017-07-24 22:57:26 -0700

    a

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var http = require('http');
+var fs = require('fs');
+
+function send404Response(response){
+	response.writeHead(404, {"Context-Type": "text/plain"});
+	response.write("Error 404: Page not found");
+	response.end();
+}
 
 function onRequest (request, response) {
 	console.log('A user made a request' + request.url);
-	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Qsy is gay");
-	response.end();
+	if(request.method == 'GET' && request.url == '/'){
+		response.writeHead(200, {"Context-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+		response.end();
+	}else{
+		send404Response(response);
+	}
 }
 http.createServer(onRequest).listen(process.env.PORT || 8080);
