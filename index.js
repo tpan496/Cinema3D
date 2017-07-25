9e629e8f703a2bda522c1b09871a11b684e9c69d
tpan496 <tianleip@andrew.cmu.edu>
2017-07-24 22:25:39 -0700

    B

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 function onRequest (request, response) {
 	console.log('A user made a request' + request.url);
 	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Here is some data");
+	response.write("Qsy is gay");
 	response.end();
 }
 http.createServer(onRequest).listen(process.env.PORT || 8080);
