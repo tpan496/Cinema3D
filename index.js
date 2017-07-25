901151ee2d74dbe641954c3a8ea29bce3eda275e
tpan496 <tianleip@andrew.cmu.edu>
2017-07-24 22:12:35 -0700

    Simple server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("test server");
+var http = require('http');
+
+function onRequest (request, response) {
+	console.log('A user made a request' + request.url);
+	response.writeHead(200, {"Context-Type": "text/plain"});
+	response.write("Here is some data");
+	response.end();
+}
+http.createServer(onRequest).listen(8888);
