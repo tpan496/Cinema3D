689a35de7bdd508d7184932e1afb0f42be9d4e60
tpan496 <tianleip@andrew.cmu.edu>
2017-07-29 17:14:55 -0700

    Chat room

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 var http = require('http');
+var mongo = require('mongodb').MongoClient;
+var client = require('socket.io').listen(8080).sockets;
 
-var server = http.createServer(function(request, response) {
+mongo.connect('mongodb://127.0.0.1/chat', function (error, db) {
+    if (error) throw error;
+    client.on('connection', function (socket) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
+        var collection = db.collection('messages'),
+            sendStatus = function(s){
+                socket.emit('status', s);
+            };
+
+        // Emit all messages
+        collection.find().limit(100).sort({_id: 1}).toArray(function(error, result){
+            if(error) throw error;
+            socket.emit('output', result);
+        })
+
+        socket.on('input', function (payload) {
+            var name = payload.name,
+                message = payload.message,
+                whitespacePattern = /^\s*$/;
+
+            if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+                sendStatus('Name and message is required');
+            } else {
+                collection.insert({ name: name, message: message }, function () {
+                    // Emit latest messages
+                    client.emit('output', [payload]);
+                    sendStatus({message: 'Message sent', clear: true});
+                })
+            }
+        });
+    })
+});
+
+var server = http.createServer(function (request, response) {
+
+    response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Qsy is gay");
 
 });
