48ba7b7b44276e693a3dadd2444b6dba826d2254
tpan496 <tianleip@andrew.cmu.edu>
2017-07-31 19:11:47 -0700

    Playlist sync

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').createServer(app);
 var port = process.env.PORT || 8080;
-var mongodbAddress = 'mongodb://tpan496:trollNoob971006!@exp-server-shard-00-00-8ecae.mongodb.net:27017,exp-server-shard-00-01-8ecae.mongodb.net:27017,exp-server-shard-00-02-8ecae.mongodb.net:27017/exp-server?ssl=true&replicaSet=exp-server-shard-0&authSource=admin'
 
 // Consts
+const mongodbAddress = 'mongodb://tpan496:trollNoob971006!@exp-server-shard-00-00-8ecae.mongodb.net:27017,exp-server-shard-00-01-8ecae.mongodb.net:27017,exp-server-shard-00-02-8ecae.mongodb.net:27017/exp-server?ssl=true&replicaSet=exp-server-shard-0&authSource=admin'
 const forceSyncThreshold = 3; // synchronize check frequency
 const whitespacePattern = /^\s*$/;
+const YT_VIDEO_ENDED = 0;
+const YT_VIDEO_PLAYING = 1;
+const YT_VIDEO_PAUSED = 2;
 
 // Only one room, so keeping the host here
 var clientIdList = [];
@@ -40,12 +43,20 @@ mongo.connect(mongodbAddress, function (error, db) {
                 socket.emit('status', s);
             };
 
-        // Display history messages in chat screen
+        // Display history messages to client
         chatMessages.find().limit(100).sort({ _id: 1 }).toArray(function (error, result) {
             if (error) throw error;
             console.log(result);
             socket.emit('new_chat_message', result);
         });
+
+        console.log('length: '+ videoUrlList.length);
+
+        // Display playlist to client
+        for(var i=0; i<videoUrlList.length; i++){
+            var payload = {url: videoUrlList[i]};
+            socket.emit('new_video_url', [payload]);
+        }
 
         // Listen for new chat message
         socket.on('user_chat_message', function (payload) {
@@ -71,18 +82,19 @@ mongo.connect(mongodbAddress, function (error, db) {
             if (whitespacePattern.test(name) || whitespacePattern.test(url)) {
                 sendStatus('Valid url is required');
             } else {
-                playlistUrls.insert({ url: url }, function () {
+
                     // Start new video and throw url into playlist
                     client.emit('new_video_url', [payload]);
                     videoHostId = socket.id;
                     videoUrlList.push(url);
-                    if (hostVideoStatus == 0) {
+
+                    // If no video is playing now
+                    if (hostYTPlayerStatus == YT_VIDEO_ENDED) {
                         videoUrlList.shift();
                         client.emit('new_video_id', { id: url });
                     }
                     console.log(url);
                     sendStatus({ message: 'Url sent', clear: true });
-                });
             }
         });
 
@@ -93,6 +105,7 @@ mongo.connect(mongodbAddress, function (error, db) {
                 if (videoHostId == null) {
                     console.log('Host appeared: ' + socket.id);
                     videoHostId = socket.id;
+                    socket.emit("host_video_progress", { time: videoHostTime, status: hostYTPlayerStatus, hostId: videoHostId });
                 }
                 socket.emit('new_video_id', { id: testVideoId });
             }
@@ -105,25 +118,25 @@ mongo.connect(mongodbAddress, function (error, db) {
             if (socket.id === videoHostId) {
                 videoHostTime = time;
                 switch (status) {
-                    case 0:
+                    case YT_VIDEO_ENDED:
                         if (videoUrlList.length > 0) {
                             var url = videoUrlList.shift();
                             client.emit('new_video_id', { id: url });
                         }
                         break;
-                    case 1:
+                    case YT_VIDEO_PLAYING:
                         if (hostYTPlayerStatus !== status) {
-                            client.emit("host_video_progress", { time: videoHostTime, status: 1 });
+                            client.emit("host_video_progress", { time: videoHostTime, status: YT_VIDEO_PLAYING });
                         } 
                         break;
-                    case 2: client.emit("host_video_progress", { time: videoHostTime, status: 2 }); break;
+                    case YT_VIDEO_PAUSED: client.emit("host_video_progress", { time: videoHostTime, status: YT_VIDEO_PAUSED }); break;
                     default: break;
                 }
                 hostYTPlayerStatus = status;
                 console.log('Host progress: ' + time);
             } else {
                 if (Math.abs(videoHostTime - time) > forceSyncThreshold) {
-                    socket.emit("host_video_progress", { time: videoHostTime, status: status });
+                    socket.emit("host_video_progress", { time: videoHostTime, status: status, hostId: videoHostId });
                 }
             }
         });
@@ -131,6 +144,7 @@ mongo.connect(mongodbAddress, function (error, db) {
         // Request to be host
         socket.on('user_host_request', function (payload) {
             videoHostId = socket.id;
+            socket.emit("host_video_progress", { time: payload.time, status: payload.status, hostId: videoHostId });
         });
     });
 });
