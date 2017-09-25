b6d2e7de2486308a984a1358def0b1ca8851bfa5
tpan496 <tianleip@andrew.cmu.edu>
2017-09-25 19:56:05 -0400

    Terry crewzzzz

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 var mongo = require('mongodb').MongoClient;
 var express = require('express');
-var app = require('express')();
-var httpServer = require('http').createServer(app);
+var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
+var app = require('express')();
+app.use(express.static('frontend'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/create-channel', function(req, res){
+    var roomName = req.body.roomName;
+
+    if(!roomList.find(function(s){return s == roomName})){
+        res.send(roomName);
+        roomList.push(roomName);
+    }else{
+        res.send('Already exists.');
+    }
+    console.log(roomList);
+});
+
+var httpServer = require('http').createServer(app);
 
 // Only one room, so keeping the host here
 var clientIdList = [];
 var videoRequestList = [];
-var currentVideoUrl = 'VtI5HM7GVGY'; // default video
+var currentVideoUrl = 'XIjzii9a4Nc'; // default video
 var hostYTPlayerStatus;
 var videoHostId;
 var videoHostTime;
+var roomList = [];
 
 exports.clientIdList = clientIdList;
 exports.videoRequestList = videoRequestList;
@@ -18,10 +35,10 @@ exports.currentVideoUrl = currentVideoUrl;
 exports.hostYTPlayerStatus = hostYTPlayerStatus;
 exports.videoHostId = videoHostId;
 exports.videoHostTime = videoHostTime;
+exports.roomList = roomList;
 
 // Server settings
 httpServer.listen(port);
-app.use(express.static('frontend'));
 
 // Connect to mongodb and respond to client events
 mongo.connect(require('./constants').mongodbAddress, function (error, db) {
